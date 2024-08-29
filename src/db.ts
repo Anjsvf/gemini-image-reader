b780bbdad72b2103dfc52e6ca262a3306435ba61
antonio santos <antoniosantos.devweb@gmail.com>
2024-08-29 17:16:58 -0300

    bug fixed

--- src/db.ts
+++ src/db.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || '', { });
+    await mongoose.connect(process.env.MONGO_URL || '', { });
     console.log('MongoDB: successful connecting');
   } catch (error) {
     console.error('connection to MongoDB fails:', error);
